15050ca2439be2ee86f5c010c91142a0505b324b
Umut Can Top <umutcantop@mail.com>
2023-06-14 12:43:18 +0300

    requirements added

--- template.py
+++ template.py
@@ -20,7 +20,8 @@ list_of_files = [
     "params.yaml",
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb"
+    "research/trials.ipynb",
+    "templates/index.html"
 ]
 
 for filepath in list_of_files:
